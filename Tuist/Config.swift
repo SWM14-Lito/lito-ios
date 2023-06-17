8b896bb1008c81e90985ab0a6b3f18bb95917b7d
Goban <gobanest@gmail.com>
2023-06-17 23:22:16 +0900

    [Setting] use plugin
    for easily use dependency path

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -2,6 +2,6 @@ import ProjectDescription
 
 let config = Config(
     plugins: [
-        .local(path: .relativeToManifest("../../Plugins/Lito")),
+        .local(path: .relativeToManifest("../../Plugins/DependencyPlugin")),
     ]
 )
