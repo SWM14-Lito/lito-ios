6dbe7ecab204b3fca933b3123e5ec027913bb3de
Goban <gobanest@gmail.com>
2023-06-26 20:17:05 +0900

    [Fix] delete unexpected x

--- Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
+++ Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -10,7 +10,7 @@ import ProjectDescription
 public extension TargetDependency {
     enum SPM {}
 }
-x
+
 public extension TargetDependency.SPM {
     static let Kingfisher = TargetDependency.external(name: "Kingfisher")
     static let Swinject = TargetDependency.external(name: "Swinject")
