1da414163ce57c95a62930ed58200acf3e7ab5cb
ddophi98 <lightcloud98@gmail.com>
2023-06-30 14:17:36 +0900

    통과하는 테스트 케이스 추가해보기

--- Projects/Presentation/Tests/PresentationTests.swift
+++ Projects/Presentation/Tests/PresentationTests.swift
@@ -1,1 +1,15 @@
-import Foundation
+import XCTest
+
+class PresentationTests: XCTestCase {
+    override func setUpWithError() throws {
+        
+    }
+    
+    override func tearDownWithError() throws {
+        
+    }
+    
+    func testExample() throws {
+        XCTAssertTrue(true)
+    }
+}
