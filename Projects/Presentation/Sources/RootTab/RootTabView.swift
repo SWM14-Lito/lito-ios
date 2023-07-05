2b3a5176cfd73e42f085de5440167a3c0e75e486
ddophi98 <lightcloud98@gmail.com>
2023-07-05 10:30:22 +0900

    [refactor] navigationstack을 최상위 뷰로 이동

--- Projects/Presentation/Sources/RootTab/RootTabView.swift
+++ Projects/Presentation/Sources/RootTab/RootTabView.swift
@@ -10,31 +10,20 @@ import SwiftUI
 
 public struct RootTabView: View {
     
-    private var coordinator: CoordinatorProtocol
     private let viewResolver: ViewResolverProtocol
-    @State private var navigationPath = NavigationPath()
     
-    public init(coordinator: CoordinatorProtocol, viewResolver: ViewResolverProtocol) {
-        self.coordinator = coordinator
+    public init(viewResolver: ViewResolverProtocol) {
         self.viewResolver = viewResolver
     }
     
     public var body: some View {
-        NavigationStack(path: $navigationPath) {
-            TabView {
-                viewResolver.resolveView(LearningHomeView.self)
-                    .tabItem { Text("학습") }
-                viewResolver.resolveView(PrevProblemCategoryView.self)
-                    .tabItem { Text("기출문제") }
-                viewResolver.resolveView(MyPageView.self)
-                    .tabItem { Text("마이페이지") }
-            }
-            .navigationDestination(for: Page.self) { page in
-                page.getView(coordinator: coordinator, viewResolver: viewResolver)
-            }
-        }
-        .onReceive(coordinator.pathPublisher) { navigationPath in
-            self.navigationPath = navigationPath
+        TabView {
+            viewResolver.resolveView(LearningHomeView.self)
+                .tabItem { Text("학습") }
+            viewResolver.resolveView(PrevProblemCategoryView.self)
+                .tabItem { Text("기출문제") }
+            viewResolver.resolveView(MyPageView.self)
+                .tabItem { Text("마이페이지") }
         }
     }
 }
