1528dcc7c500c55588788bf7b7f962288ed2ca20
Goban <gobanest@gmail.com>
2023-07-10 14:23:39 +0900

    [chore] 필요없는 keychain read 제거

--- Projects/Domain/Sources/Services/ProfileSettingUseCase.swift
+++ Projects/Domain/Sources/Services/ProfileSettingUseCase.swift
@@ -22,26 +22,14 @@ public final class DefaultProfileSettingUseCase: ProfileSettingUseCase {
     }
     
     public func postProfileInfo(profileInfoDTO: ProfileInfoDTO) -> AnyPublisher<Void, Error> {
-        guard let accessToken = KeyChainManager.read(key: .accessToken) else {
-            return Fail(error: ErrorVO.fatalError)
-                .eraseToAnyPublisher()
-        }
         return repository.postProfileInfo(profileInfoDTO: profileInfoDTO)
     }
     
     public func postProfileImage(profileImageDTO: ProfileImageDTO) -> AnyPublisher<Void, Error> {
-        guard let accessToken = KeyChainManager.read(key: .accessToken) else {
-            return Fail(error: ErrorVO.fatalError)
-                .eraseToAnyPublisher()
-        }
         return repository.postProfileImage(profileImageDTO: profileImageDTO)
     }
     
     public func postAlarmAcceptance(alarmAcceptanceDTO: AlarmAcceptanceDTO) -> AnyPublisher<Void, Error> {
-        guard let accessToken = KeyChainManager.read(key: .accessToken) else {
-            return Fail(error: ErrorVO.fatalError)
-                .eraseToAnyPublisher()
-        }
         return repository.postAlarmAcceptance(alarmAcceptanceDTO: alarmAcceptanceDTO)
     }
 }
