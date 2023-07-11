2e4c4fac6e60f9709e9c89679cbb23c2ccfef822
Goban <gobanest@gmail.com>
2023-07-11 16:56:03 +0900

    [setting] tuist signing (mobile Provisioning)

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -5,7 +5,7 @@ public extension Project {
         name: String,
         platform: Platform = .iOS,
         product: Product,
-        organizationName: String = "Lito",
+        organizationName: String = "com.lito",
         packages: [Package] = [],
         deploymentTarget: DeploymentTarget? = .iOS(targetVersion: "15.0", devices: [.iphone]),
         dependencies: [TargetDependency] = [],
