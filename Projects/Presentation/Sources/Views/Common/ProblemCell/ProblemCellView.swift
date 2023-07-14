160ca8e9adfa700e55038c1058d87d92b0caeb74
Goban <gobanest@gmail.com>
2023-07-14 14:11:18 +0900

    [chore] 접근 제어

--- Projects/Presentation/Sources/Views/Common/ProblemCell/ProblemCellView.swift
+++ Projects/Presentation/Sources/Views/Common/ProblemCell/ProblemCellView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 public struct ProblemCellView: View {
     
-    @StateObject var viewModel: ProblemCellViewModel
+    @StateObject private var viewModel: ProblemCellViewModel
     
     public init(viewModel: ProblemCellViewModel) {
         self._viewModel = StateObject(wrappedValue: viewModel)
