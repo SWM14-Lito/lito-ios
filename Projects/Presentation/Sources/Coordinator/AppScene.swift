ec5806ee1f51c8010f48c8a8d942f6528a0dec4d
Goban <gobanest@gmail.com>
2023-08-04 16:45:57 +0900

    [chore] 불필요한 부분 제거

--- Projects/Presentation/Sources/Coordinator/AppScene.swift
+++ Projects/Presentation/Sources/Coordinator/AppScene.swift
@@ -10,7 +10,7 @@ import SwiftUI
 import KakaoSDKAuth
 import Domain
 
-public enum AppScene: Identifiable {
+public enum AppScene: Identifiable, Hashable {
     case loginScene, profileSettingScene(userAuthVO: UserAuthVO)
     case rootTabScene
     case learningHomeScene, problemListScene, solvingProblemListScene, favoriteProblemListScene, problemDetailScene(id: Int), chattingScene, problemSearchScene
@@ -21,55 +21,4 @@ public enum AppScene: Identifiable {
         return self
     }
     
-}
-
-extension AppScene: Hashable {
-    
-    public func hash(into hasher: inout Hasher) {
-        switch self {
-        case .loginScene:
-            hasher.combine(0)
-        case let .profileSettingScene(userAuthVO):
-            hasher.combine(1)
-            hasher.combine(userAuthVO)
-        case .rootTabScene:
-            hasher.combine(2)
-        case .learningHomeScene:
-            hasher.combine(3)
-        case .problemListScene:
-            hasher.combine(4)
-        case .solvingProblemListScene:
-            hasher.combine(5)
-        case .favoriteProblemListScene:
-            hasher.combine(6)
-        case let .problemDetailScene(id):
-            hasher.combine(7)
-            hasher.combine(id)
-        case .chattingScene:
-            hasher.combine(8)
-        case .problemSearchScene:
-            hasher.combine(9)
-        case .pedigreeListScene:
-            hasher.combine(10)
-        case .myPageScene:
-            hasher.combine(11)
-        }
-    }
-    
-    public static func == (lhs: AppScene, rhs: AppScene) -> Bool {
-        switch (lhs, rhs) {
-        case (.loginScene, .loginScene),
-            (.rootTabScene, .rootTabScene),
-            (.learningHomeScene, .learningHomeScene):
-            // Add other cases here for the scenes that don't have associated values
-            return true
-        case let (.profileSettingScene(userAuthVO1), .profileSettingScene(userAuthVO2)):
-            return userAuthVO1 == userAuthVO2
-        case let (.problemDetailScene(id1), .problemDetailScene(id2)):
-            return id1 == id2
-            // Add other cases for the scenes with associated values
-        default:
-            return false
-        }
-    }
 }
