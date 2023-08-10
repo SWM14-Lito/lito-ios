b035eaadcfea197cec3f6dda1ca2ab59e2156e9a
Goban <gobanest@gmail.com>
2023-08-10 20:55:10 +0900

    [fix] 실수로 제거한 코드 복구

--- Projects/Presentation/Sources/Views/Common/FilterView.swift
+++ Projects/Presentation/Sources/Views/Common/FilterView.swift
@@ -118,7 +118,7 @@ struct FilterView<T: FilterComponent>: View {
                     }
                     .background(
                         RoundedRectangle(cornerRadius: 19 )
-                            .fill()
+                            .fill(selectedFilter == filter ? .Button_Point : .Bg_Light)
                     )
                 }
             }
