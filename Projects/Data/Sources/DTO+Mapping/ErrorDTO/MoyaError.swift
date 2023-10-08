e2e9340934671376e4ecac19454721984408a2a7
Goban <gobanest@gmail.com>
2023-10-08 13:08:03 +0900

    [fix] nickname 한글 허용 및 에러 핸들링 수정

--- Projects/Data/Sources/DTO+Mapping/ErrorDTO/MoyaError.swift
+++ Projects/Data/Sources/DTO+Mapping/ErrorDTO/MoyaError.swift
@@ -13,50 +13,63 @@ import Domain
 extension MoyaError {
     
     /*
-    /// Response의 data를 이미지로 맵핑하는 과정에서 발생할 수 있는 에러
-    case imageMapping(Response)
-
-    /// Response의 data를 json 으로 맵핑하는 과정에서 발생할 수 있는 에러
-    case jsonMapping(Response)
-
-    /// Response의 data를 string 으로 맵핑하는 과정에서 발생할 수 있는 에러
-    case stringMapping(Response)
-
-    /// Response의 data를 특정 타입으로 파싱하는 과정에서 발생할 수 있는 에러
-    case objectMapping(Swift.Error, Response)
-
-    /// URLRequest를 만들때 httpBody 를 인코딩하는 과정에서 발생할 수 있는 에러
-    case encodableMapping(Swift.Error)
-
-    /// Response 의 statusCode가 설정한 statusCode에 포함되지 않는다면 발생하는 에러
-    case statusCode(Response)
-
-    /// URLRequest 요청, 수행과정에서 발생할 수 있는 에러
-    case underlying(Swift.Error, Response?)
-
-    /// URLRequest를 만들때 String 을 URL로 변환하는 과정에서 발생할 수 있는 에러
-    case requestMapping(String)
-
-    /// URLRequest를 만들때 parameter 를 인코딩하는 과정에서 발생할 수 있는 에러
-    case parameterEncoding(Swift.Error)
-    */
+     /// Response의 data를 이미지로 맵핑하는 과정에서 발생할 수 있는 에러
+     case imageMapping(Response)
+     
+     /// Response의 data를 json 으로 맵핑하는 과정에서 발생할 수 있는 에러
+     case jsonMapping(Response)
+     
+     /// Response의 data를 string 으로 맵핑하는 과정에서 발생할 수 있는 에러
+     case stringMapping(Response)
+     
+     /// Response의 data를 특정 타입으로 파싱하는 과정에서 발생할 수 있는 에러
+     case objectMapping(Swift.Error, Response)
+     
+     /// URLRequest를 만들때 httpBody 를 인코딩하는 과정에서 발생할 수 있는 에러
+     case encodableMapping(Swift.Error)
+     
+     /// Response 의 statusCode가 설정한 statusCode에 포함되지 않는다면 발생하는 에러
+     case statusCode(Response)
+     
+     /// URLRequest 요청, 수행과정에서 발생할 수 있는 에러
+     case underlying(Swift.Error, Response?)
+     
+     /// URLRequest를 만들때 String 을 URL로 변환하는 과정에서 발생할 수 있는 에러
+     case requestMapping(String)
+     
+     /// URLRequest를 만들때 parameter 를 인코딩하는 과정에서 발생할 수 있는 에러
+     case parameterEncoding(Swift.Error)
+     */
     
     public func toVO() -> ErrorVO {
         switch self {
         case .underlying(let error, let response):
-            if let response = response, response.statusCode == 500 {
-                do {
-                    let serverErrorMessage = try convertServerErrorMessage(response: response)
-                    return .retryableError(serverErrorMessage.errors[0].reason)
-                } catch {
-                    return .retryableError("일시적인 서버 에러입니다. 잠시 후 다시 시도해주세요.")
-                }
-            }
             // 토큰 재발급 실패
             if let afError = error.asAFError, afError.isRequestRetryError {
                 if case .requestRetryFailed = afError {
                     if response?.statusCode == 401 {
                         return .tokenExpired
+                    }
+                }
+            }
+            if let response = response {
+                if response.statusCode == 500 {
+                    do {
+                        let serverErrorMessage = try convertServerErrorMessage(response: response)
+                        return .retryableError(serverErrorMessage.errors[0].reason)
+                    } catch {
+                        return .retryableError("일시적인 서버 에러입니다. 잠시 후 다시 시도해주세요.")
+                    }
+                }
+                // 리팩토링 필요.
+                if response.statusCode == 400 {
+                    do {
+                        let serverErrorMessage = try convertServerErrorMessage(response: response)
+                        if serverErrorMessage.code == "U002" {
+                            return .retryableError(serverErrorMessage.message)
+                        }
+                    } catch {
+                        return .fatalError
                     }
                 }
             }
@@ -102,9 +115,9 @@ extension MoyaError {
                 \(serverErrorMessage.description)
                 """
             } catch {
-                #if DEBUG
+#if DEBUG
                 print("Failed to Parse ServerErrorMessage \n \(error)")
-                #endif
+#endif
                 debugDescription = response.debugDescription
                 if dataParse {
                     parsedData = String(data: response.data, encoding: .utf8) ?? "Empty"
